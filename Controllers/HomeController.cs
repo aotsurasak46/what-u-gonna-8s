fa70385ba5b870594984b8d9a12ef6569c46e0c9
aotsurasak46 <aotsurasak46@gmail.com>
2023-04-21 15:30:38 +0700

    add profile

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Diagnostics;
 using System.Security.Claims;
 using Microsoft.AspNetCore.Mvc;
+using what_u_gonna_eat.Data;
 using what_u_gonna_eat.Models;
 
 namespace what_u_gonna_eat.Controllers;
@@ -8,15 +9,33 @@ namespace what_u_gonna_eat.Controllers;
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly AccountDbContext _db;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger,AccountDbContext db)
     {
         _logger = logger;
+        _db = db;
     }
 
     public IActionResult Index()
     {
-        return View();
+        var userId = HttpContext.Session.GetInt32("UserId");
+        if (userId != null)
+        {
+            // use the user ID to retrieve user information
+            var user = _db.Account.FirstOrDefault(u => u.Id == userId);
+            if (user != null)
+            {
+                ViewBag.Id = user.Id;
+                ViewBag.Username = user.Username;
+                ViewBag.Email = user.Email;
+            }
+            return View(user);
+        }
+        else
+        {
+            return RedirectToAction("Login", "Account");
+        }
     }
 
     public IActionResult Privacy()
