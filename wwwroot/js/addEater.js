fbd73ad02941c62f7ce9b8e2f856215ea2b3e2cd
aotsurasak46 <aotsurasak46@gmail.com>
2023-04-28 17:55:55 +0700

    add confirm,reverse post

---
+++ wwwroot/js/addEater.js
@@ -0,0 +1,24 @@
+﻿// Get the modal element
+const modal = document.getElementById("modal");
+
+// get button
+const btn = document.getElementById("open-modal-btn");
+//get close
+const span = document.getElementsByClassName("close")[0];
+
+//when onclick
+btn.onclick = function () {
+    modal.style.display = "block";
+};
+
+// when click x 
+span.onclick = function () {
+    modal.style.display = "none";
+};
+
+//click out x
+// window.onclick = function (event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
+// };
