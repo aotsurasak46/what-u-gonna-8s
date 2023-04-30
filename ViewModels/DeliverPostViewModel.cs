821e0d8407bd393c294d8384741bff4cc42df8b8
erum <heartof.effort@gmail.com>
2023-04-30 17:00:05 +0700

    add

--- ViewModels/DeliverPostViewModel.cs
+++ ViewModels/DeliverPostViewModel.cs
@@ -5,6 +5,7 @@ namespace what_u_gonna_eat.ViewModels
     public class DeliverPostView
     {
         public Account account { get; set; }
+        public List<Account> accounts { get; set; }
         public DeliverPost deliverPost { get; set; }
         public List<DeliverPost> deliverPosts { get; set; }
         public Order Order { get; set; }
